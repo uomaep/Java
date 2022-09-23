913adc357a711674b7d69eeced9156334054512f
uomaep <114221785+uomaep@users.noreply.github.com>
2022-09-24 01:47:44 +0900

    Add files via upload

---
+++ CodeUp/P1099.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class P1099 {
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		int box[][] = new int[10][10];
+		boolean stop = false;
+		
+		for(int i=0; i<box.length; i++) {
+			for(int j=0; j<box[i].length; j++) {
+				box[i][j] = in.nextInt();
+			}
+		}
+		
+		
+		for(int i=1; i<=box.length-2; i++) {
+			for(int j=1; i<=box[i].length-2; j++) {
+				if(box[i][j] == 0) box[i][j] = 9;
+				else if(box[i][j] == 1) {
+					j-=2;
+					i++;
+					continue;
+				}
+				else if(box[i][j] == 2) {
+					box[i][j] = 9;
+					stop = true;
+					break;
+				}
+			}
+			if(stop) break;
+		}
+		
+		for(int i=0; i<box.length; i++) {
+			for(int j=0; j<box[i].length; j++) {
+				System.out.print(box[i][j] + " ");
+			}
+			System.out.println();
+		}
+		
+
+	}
+
+}
