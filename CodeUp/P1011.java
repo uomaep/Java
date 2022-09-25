a2199b6cd6441cb51befbf96f17a1f6ba1f88e26
Daewon Lim <114221785+uomaep@users.noreply.github.com>
2022-09-25 12:45:08 +0900

    Add files via upload

---
+++ CodeUp/P1011.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class P1011 {
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		char ch;
+		
+		ch = in.next().charAt(0);
+		System.out.println(ch);
+
+	}
+
+}
