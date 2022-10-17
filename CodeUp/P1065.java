ae1e2ce505d244b6177d5e2e20e74b1c95243a7e
Daewon Lim <114221785+uomaep@users.noreply.github.com>
2022-10-17 16:13:54 +0900

    Add files via upload

---
+++ CodeUp/P1065.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class P1065 {
+
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+		int a = in.nextInt();
+		int b = in.nextInt();
+		int c = in.nextInt();
+		
+		if(a%2 == 0) System.out.println(a);
+		if(b%2 == 0) System.out.println(b);
+		if(c%2 == 0) System.out.println(c);
+		
+	}
+
+}
