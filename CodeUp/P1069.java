ddfc7381c81a395c9b5655e5e34817e68926e9ba
Daewon Lim <114221785+uomaep@users.noreply.github.com>
2022-10-17 16:30:39 +0900

    Add files via upload

---
+++ CodeUp/P1069.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class P1069 {
+
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+		char ch = in.next().charAt(0);
+		
+		switch (ch) {
+		case 'A':
+			System.out.println("best!!!");
+			break;
+		case 'B':
+			System.out.println("good!!");
+			break;
+		case 'C':
+			System.out.println("run!");
+			break;
+		case 'D':
+			System.out.println("slowly~");
+			break;
+		default:
+			System.out.println("what?");
+			break;
+		}
+		
+	}
+
+}
